1106017b52b2ec2a0f00814c522510a62cc40c9b
anatolytimonin <anatolytimonin8@gmail.com>
2024-12-09 08:22:22 +0200

    feat(lsp): add support for LSP integration

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -22,3 +22,10 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+-- c/cpp
+lspconfig.clangd.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
